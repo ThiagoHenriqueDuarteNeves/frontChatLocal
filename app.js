f9fed85cfee0bbfe7c234c7c16fea33efb09bc20
Thiago Neves <ThiagoHenriqueDuarteNeves@users.noreply.github.com>
2025-03-28 21:46:05 -0300

    Tunelamento
    Tunelamento ngrok 
    
    Signed-off-by: Thiago Neves <ThiagoHenriqueDuarteNeves@users.noreply.github.com>

--- app.js
+++ app.js
@@ -89,7 +89,7 @@ document.addEventListener("DOMContentLoaded", function () {
         const botMessageElem = appendMessage("Gepetudo", "Pensando...", true, true);
 
         try {
-            const response = await fetch("https://5a9a-177-69-1-37.ngrok-free.app/api/v0/chat/completions", {
+            const response = await fetch("https://5653-177-69-1-37.ngrok-free.app/api/v0/chat/completions", {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify({
